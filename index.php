d4991e193f5523a63f093d9a881e9fa80a46e7ca
Rebecca Dicharry <rdicharry@gmail.com>
2016-09-22 13:27:32 -0600

    a first little form for entering a plant name - does nothing, yet

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php
-echo "Hello World!";
+echo "Enter a plant name:";
+$commonName = "";
+$botanicalName = "";
+if(isset($_POST['common-name'])) $commonName = $_POST['common-name'];
+if(isset($_POST['botanical-name'])) $botanicalName = $_POST['botanical-name'];
+
+echo <<<_END
+<html>
+<head>
+<title>Plant Names</title>
+</head>
+<body>
+<form method="post" action="index.php">
+Common Name
+<input type="text" name="common-name">
+<br>
+Botanical (Latin) Name
+<input type="text" name="botanical-name">
+<br>
+<input type="submit" >
+</form>
+_END;
+
+if(strlen($commonName) > 0 || strlen($botanicalName) > 0) {
+	echo <<<_END
+	<div>
+	$commonName:: <em > $botanicalName</em >
+</div >
+_END;
+}
+	echo <<<_END
+</body>
+</html>
+
+_END;
+
 ?>
